383a00a0f9029d3bad0ea8d0fa2a47547fd8ad81
David X. Lau <richliu@richliudeiMac.local>
2016-05-23 10:51:09 +0800

    add demo

---
+++ demo/GYBootingProtectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYBootingProtectionDemo
+//
+//  Created by richliu on 16/5/20.
+//  Copyright © 2016年 richliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demo/GYBootingProtectionDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GYBootingProtectionDemo
+//
+//  Created by richliu on 16/5/20.
+//  Copyright © 2016年 richliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
